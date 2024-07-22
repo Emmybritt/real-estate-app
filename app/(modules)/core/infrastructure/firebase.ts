11532c0bb64aa5cfa46104b223ebb40bfce8a055
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2024-07-23 00:23:22 +0100

    format file

--- app/(modules)/core/infrastructure/firebase.ts
+++ app/(modules)/core/infrastructure/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-console.log(process.env)
+console.log(process.env);
 const firebaseConfig = {
   apiKey: process.env.NEXT_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
