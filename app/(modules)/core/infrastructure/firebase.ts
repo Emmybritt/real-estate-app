35d3753eb62a8cca7f672d94212b55253498c667
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2024-07-23 00:31:18 +0100

    fix issues

--- app/(modules)/core/infrastructure/firebase.ts
+++ app/(modules)/core/infrastructure/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 console.log(process.env);
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
